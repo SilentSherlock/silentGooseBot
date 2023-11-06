06a176dbca1f2729f9e35fa4ccb010098541039b
SilentSherlock <silentsherlock1998@gmail.com>
2023-11-06 23:14:09 +0800

    1.add entities and mappers, modify config

--- src/main/java/com/silent/silentgoosebot/SilentGooseBotApplication.java
+++ src/main/java/com/silent/silentgoosebot/SilentGooseBotApplication.java
@@ -1,9 +1,12 @@
 package com.silent.silentgoosebot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.silent.silentgoosebot.dao")
 public class SilentGooseBotApplication {
 
     public static void main(String[] args) {
