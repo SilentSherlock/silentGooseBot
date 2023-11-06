8a672b148149f7b5b4a284cb0c69f97945a32ec3
SilentSherlock <silentsherlock1998@gmail.com>
2023-11-06 20:23:48 +0800

    1.git repo initial

---
+++ src/main/java/com/silent/silentgoosebot/SilentGooseBotApplication.java
@@ -0,0 +1,13 @@
+package com.silent.silentgoosebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilentGooseBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilentGooseBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silent/silentgoosebot/SilentGooseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silent.silentgoosebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentGooseBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
