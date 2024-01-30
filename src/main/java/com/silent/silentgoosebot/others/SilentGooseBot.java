f58030e656849cc72204c261c0c66ba35fe5cb07
SilentSherlock <1536141515@qq.com>
2024-01-30 18:24:04 +0800

    1.add welcome method

--- src/main/java/com/silent/silentgoosebot/others/SilentGooseBot.java
+++ src/main/java/com/silent/silentgoosebot/others/SilentGooseBot.java
@@ -2,6 +2,10 @@ package com.silent.silentgoosebot.others;
 
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.MessageContext;
+import org.telegram.abilitybots.api.objects.Privacy;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -23,23 +27,23 @@ public class SilentGooseBot extends AbilityBot {
         super(botToken, botUsername, options);
     }
 
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-        log.info("bot receive message, begin distribute");
-        updates.stream()
-                .filter(Update::hasMessage)
-                .map(Update::getMessage)
-                .forEach(message -> {
-                    Chat curChat = message.getChat();
-                    log.info("curChat " + curChat.toString());
-                    log.info("curMsg " + message);
-                    if (curChat.isChannelChat()) {
-                        log.info("channel chat");
-                    } else if (curChat.isUserChat()) {
-                        log.info("user chat");
-                    }
-                });
-    }
+//    @Override
+//    public void onUpdatesReceived(List<Update> updates) {
+//        log.info("bot receive message, begin distribute");
+//        updates.stream()
+//                .filter(Update::hasMessage)
+//                .map(Update::getMessage)
+//                .forEach(message -> {
+//                    Chat curChat = message.getChat();
+//                    log.info("curChat " + curChat.toString());
+//                    log.info("curMsg " + message);
+//                    if (curChat.isChannelChat()) {
+//                        log.info("channel chat");
+//                    } else if (curChat.isUserChat()) {
+//                        log.info("user chat");
+//                    }
+//                });
+//    }
 
     @Override
     public void onClosing() {
@@ -51,5 +55,27 @@ public class SilentGooseBot extends AbilityBot {
         return 0;
     }
 
+    /**
+     * called when new member join group
+     * group
+     * @return
+     */
+    public Ability welcome() {
+        return Ability.builder()
+                .name("welcome")
+                .info("welcome our new member")
+                .locality(Locality.GROUP) //used in group
+                .privacy(Privacy.PUBLIC) //access by every member
+                .action(this::welcomeNewMember)
+                .build();
+    }
 
+
+    private void welcomeNewMember(MessageContext messageContext) {
+        log.info("welcome new member");
+        Update update = messageContext.update();
+        if (update.hasMessage() && update.getMessage().getNewChatMembers() != null) {
+
+        }
+    }
 }
