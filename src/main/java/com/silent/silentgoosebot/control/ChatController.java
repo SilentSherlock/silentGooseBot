d7af69125825217930ccdc43768a64f823ec3d0a
SilentSherlock <1536141515@qq.com>
2024-08-27 18:05:42 +0800

    1.add chat api

--- src/main/java/com/silent/silentgoosebot/control/ChatController.java
+++ src/main/java/com/silent/silentgoosebot/control/ChatController.java
@@ -3,10 +3,15 @@ package com.silent.silentgoosebot.control;
 import com.silent.silentgoosebot.others.MoistLifeApp;
 import com.silent.silentgoosebot.others.base.AppAccountMap;
 import com.silent.silentgoosebot.others.base.Result;
+import com.silent.silentgoosebot.service.ChatService;
+import it.tdlight.jni.TdApi;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,12 +20,32 @@ public class ChatController {
     @Resource
     private AppAccountMap appAccountMap;
 
+    @Resource
+    private ChatService chatService;
+
     @RequestMapping("/getUserChats")
     public Result getUserChats(String phoneNumber) {
         MoistLifeApp curAccount = appAccountMap.getAccountMap().get(phoneNumber);
         if (curAccount == null) {
             log.info("current account is null {}", phoneNumber);
+            return Result.createByFalse("without app as " + phoneNumber);
         }
 
+        List<TdApi.Chat> chats = chatService.getClientChats(curAccount);
+        List<TdApi.Chat> userChats = new ArrayList<>();
+        List<TdApi.Chat> groupChats = new ArrayList<>();
+        List<TdApi.Chat> channelChats = new ArrayList<>();
+        for (TdApi.Chat chat : chats) {
+            TdApi.ChatType chatType = chat.type;
+            if (chatType instanceof TdApi.ChatTypeBasicGroup || chatType instanceof TdApi.ChatTypeSupergroup) {
+                if (chatType instanceof TdApi.ChatTypeSupergroup && ((TdApi.ChatTypeSupergroup) chatType).isChannel) {
+                    channelChats.add(chat);
+                } else {
+                    groupChats.add(chat);
+                }
+            } else if (chatType instanceof TdApi.ChatTypePrivate || chatType instanceof TdApi.ChatTypeSecret) {
+                userChats.add(chat);
+            }
+        }
     }
 }
